9160430194e12c6129ae0e1548ef8641d92dcc26
Muro Bakuridze <78916692+MuroBakuridze@users.noreply.github.com>
2022-12-06 20:09:02 +0400

    Request password & reset password pages ++

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -2,6 +2,8 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Login from "../views/Login.vue";
 import Dashboard from "../views/Dashboard.vue";
+import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
 
 const routes = [
     {
@@ -16,7 +18,23 @@ const routes = [
         meta: {
           requiresGuest: true
         }
-      },
+    },
+    {
+      path: '/request-password',
+      name: 'requestPassword',
+      component: RequestPassword,
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: '/reset-password/:token',
+      name: 'resetPassword',
+      component: ResetPassword,
+      meta: {
+        requiresGuest: true
+      }
+    },
 ];
 
 const router = createRouter({

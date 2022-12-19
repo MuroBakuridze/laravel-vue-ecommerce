43ba92d4cd2976aec0b14367badb82b852d8903d
Muro Bakuridze <78916692+MuroBakuridze@users.noreply.github.com>
2022-12-19 23:50:52 +0400

    Products CRUD implement +

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(),
+            'image' => $this->faker->imageUrl(),
+            'description' => $this->faker->realText(2000),
+            'price' => $this->faker->randomFloat(2, 20, 5000),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'created_by' => 1,
+            'updated_by' => 1,
+        ];
+    }
+}

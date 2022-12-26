38c5b1a98730b85ce675ff8735ac63803c0273c0
Muro Bakuridze <78916692+MuroBakuridze@users.noreply.github.com>
2022-12-26 20:53:04 +0400

    Implement Per Page and Search in Products

--- backend/src/views/Products.vue
+++ backend/src/views/Products.vue
@@ -64,36 +64,36 @@
             </tr>
           </tbody>
         </table>
-        <div v-if="!products.loading" class="flex justify-between items-center mt-5">
-          <div v-if="products.data.length">
+        <div v-if="!products.loading" class="flex justify-between items-center mt-5 w-full ">
+          <div v-if="products.data.length" class="flex-start">
             Showing from {{ products.from }} to {{ products.to }}
           </div>
-            <nav
-              v-if="products.total > products.limit"
-              class="relative z-0 inline-flex justify-center rounded-md shadow-sm -space-x-px"
-              aria-label="Pagination"
+          <nav
+            v-if="products.total > products.limit"
+            class="relative z-0 inline-flex justify-center rounded-md shadow-sm -space-x-px flex-end"
+            aria-label="Pagination"
+          >
+            <!-- Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" -->
+            <a
+              v-for="(link, i) of products.links"
+              :key="i"
+              :disabled="!link.url"
+              href="#"
+              @click.prevent="getForPage($event, link)"
+              aria-current="page"
+              class="relative inline-flex items-center px-4 py-2 border text-sm font-medium whitespace-nowrap"
+              :class="[
+                  link.active
+                    ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
+                    : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
+                  i === 0 ? 'rounded-l-md' : '',
+                  i === products.links.length - 1 ? 'rounded-r-md' : '',
+                  !link.url ? ' bg-gray-100 text-gray-700': ''
+                ]"
+              v-html="link.label"
             >
-              <!-- Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" -->
-              <a
-                v-for="(link, i) of products.links"
-                :key="i"
-                :disabled="!link.url"
-                href="#"
-                @click.prevent="getForPage($event, link)"
-                aria-current="page"
-                class="relative inline-flex items-center px-4 py-2 border text-sm font-medium whitespace-nowrap"
-                :class="[
-                    link.active
-                      ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
-                      : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
-                    i === 0 ? 'rounded-l-md' : '',
-                    i === products.links.length - 1 ? 'rounded-r-md' : '',
-                    !link.url ? ' bg-gray-100 text-gray-700': ''
-                  ]"
-                v-html="link.label"
-              >
-              </a>
-            </nav>
+            </a>
+          </nav>
         </div>
       </template>
     </div>
@@ -115,7 +115,11 @@ onMounted(() => {
 })
 
 function getProducts(url = null) {
-  store.dispatch("getProducts", {url});
+  store.dispatch("getProducts", {
+    url,
+    search: search.value,
+    perPage: perPage.value,
+  });
 }
 
 function getForPage(ev, link) {
